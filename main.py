93b6d3b0bf2653fa463ca72db28b4a740adfa309
BrianDo1 <bradm111@hotmail.com>
2018-07-16 14:43:22 -0700

    New Branch

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def main(msg):
-    #adding anther comment
-    #adding 2nd comment of file
     #print message
     print(msg)
+
+def secondary():
+    print("New Function")
 
 main ("Hello People")
