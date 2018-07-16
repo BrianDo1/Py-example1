42a31d55346ab3fa17453110814fabb6a739c9c2
BrianDo1 <bradm111@hotmail.com>
2018-07-16 14:32:08 -0700

    Corrected Comments

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def main(msg):
-    //adding 2nd comment of file
-    //print message
+    #adding 2nd comment of file
+    #print message
     print(msg)
 
 main ("Hello People")
